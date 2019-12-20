8ad805e69f79a0a1696c3825a91f4572e9741bf5
baf62495 <baf62495@aol.com>
2019-12-20 15:08:18 -0500

    tests GET /apps

--- test/app.test.js
+++ test/app.test.js
@@ -17,4 +17,66 @@ describe('GET /apps', () => {
                 )
             })
     })
+
+    it('should return 400 if sort query is incorrect', () => {
+        return supertest(app)
+            .get('/apps')
+            .query({ sort: 'MISTAKE' })
+            .expect(400, 'Sort must be one of the following: rating, app')
+    })
+
+    it('should return 400 if genre query is incorrect', () => {
+        return supertest(app)
+            .get('/apps')
+            .query({ genres: 'MISTAKE' })
+            .expect(400, 'Genres must be one of the folling: Action, Puzzle, Strategy, Casual, Arcade, Card')
+    })
+
+    it('should sort by App', () => {
+        return supertest(app)
+            .get('/apps')
+            .query({ sort: 'App' })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => {
+                expect(res.body).to.be.an('array')
+                let sorted = true
+
+                let i = 0
+                while (i < res.body.length - 1) {
+                    const appAtI = res.body[i]
+                    const appAtIPlus1 = res.body[i + 1]
+                    if (appAtIPlus1.App > appAtI.App) {
+                    sorted = false
+                    break
+                    }
+                    i++
+                }
+                expect(sorted).to.be.true
+            })
+    })
+
+    it('should sort by Rating', () => {
+        return supertest(app)
+            .get('/apps')
+            .query({ sort: 'Rating' })
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(res => {
+                expect(res.body).to.be.an('array')
+                let sorted = true
+
+                let i = 0
+                while (i < res.body.length - 1) {
+                    const appAtI = res.body[i]
+                    const appAtIPlus1 = res.body[i + 1]
+                    if (appAtIPlus1.Rating > appAtI.Rating) {
+                    sorted = false
+                    break
+                    }
+                    i++
+                }
+                expect(sorted).to.be.true
+            })
+    })
 })
